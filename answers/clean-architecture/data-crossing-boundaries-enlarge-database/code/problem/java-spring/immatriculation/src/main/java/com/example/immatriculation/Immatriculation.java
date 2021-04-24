68c8605b7dc54d9fc23f77f815a877467a89a278
maxime <deme@octo.com>
2021-04-24 14:11:28 +0200

    wip

---
+++ answers/clean-architecture/data-crossing-boundaries-enlarge-database/code/problem/java-spring/immatriculation/src/main/java/com/example/immatriculation/Immatriculation.java
@@ -0,0 +1,13 @@
+package com.example.immatriculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Immatriculation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Immatriculation.class, args);
+    }
+
+}
